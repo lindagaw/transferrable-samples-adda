d2c9fbc6f007e8fbe5ca9ad332017f37fef8794a
Ashley Gao <gaoye0718@gmail.com>
2021-10-16 17:23:01 -0400

    WIP

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 """Main script for ADDA."""
-
+import pretty_errors
 import params
+
+from torchvision import datasets, transforms, models
+
+import torch.nn as nn
+
 from core import eval_src, eval_tgt, train_src, train_tgt
 from models import Discriminator, LeNetClassifier, LeNetEncoder
 from utils import get_data_loader, init_model, init_random_seed
@@ -16,12 +21,14 @@ if __name__ == '__main__':
     tgt_data_loader_eval = get_data_loader(params.tgt_dataset, train=False)
 
     # load models
-    src_encoder = init_model(net=LeNetEncoder(),
-                             restore=params.src_encoder_restore)
-    src_classifier = init_model(net=LeNetClassifier(),
-                                restore=params.src_classifier_restore)
-    tgt_encoder = init_model(net=LeNetEncoder(),
-                             restore=params.tgt_encoder_restore)
+    progenitor = models.resnet50(pretrained=True)
+    progenitor.fc = torch.nn.Linear(2048, 10)
+    progenitor = progenitor.to(torch.device('cuda:0'))
+
+    src_encoder = torch.nn.Sequential(*(list(progenitor.children())[:-1]))
+    tgt_encoder = torch.nn.Sequential(*(list(progenitor.children())[:-1]))
+    src_classifier = torch.nn.Linear(2048, 10)
+
     critic = init_model(Discriminator(input_dims=params.d_input_dims,
                                       hidden_dims=params.d_hidden_dims,
                                       output_dims=params.d_output_dims),
