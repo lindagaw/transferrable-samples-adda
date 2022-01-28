57e0219577f91ab65f1b84550e3def931a69cba3
Ashley Gao <ashleygao@Ashleys-MacBook-Pro.local>
2022-01-27 21:22:31 -0500

    WIP

--- params.py
+++ params.py
@@ -39,7 +39,7 @@ save_step = 100
 manual_seed = None
 
 # params for optimizing models
-d_learning_rate = 1e-7
-c_learning_rate = 1e-7
+d_learning_rate = 1e-6
+c_learning_rate = 1e-6
 beta1 = 0.5
 beta2 = 0.9
